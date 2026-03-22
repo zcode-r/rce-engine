19bd16f93a54ded93b5a9d256b3ece5206b1d19d
G-BAT <batman02970@gmail.com>
2026-03-22 22:41:29 +0530

    made changes

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM alpine:latest
-RUN apk add --no-cache python3 g++
-
-WORKDIR /app
-RUN adduser -D runner
-USER runner
--- prisma/migrations/20260217170649_add_professional_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Submission" ADD COLUMN     "error" TEXT,
-ADD COLUMN     "output" TEXT,
-ADD COLUMN     "title" TEXT;
